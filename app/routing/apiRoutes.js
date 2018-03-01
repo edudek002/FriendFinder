f8c5a77dd98352d1e608f47eb9200116c2a6d20a
Edyta <dudek002@hotmail.com>
2018-03-01 16:03:50 -0600

    logic working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,42 +7,39 @@
 // Sets up the Express App
 // =============================================================
 
-
-
-//console.log("Comes from server.js " + myData);
-//console.log(myData.name);
-//console.log(myData.q1);
-//console.log(parseInt(myData.q1));
-
-
-
 var friendsData = require("../data/friends.js");
 
-console.log(friendsData);
+var friends = require("../data/friends.js");
 
-console.log(friendsData[1]);
-console.log(friendsData[1].name);
-console.log(friendsData[1].q1);
-console.log(friendsData[1].q2);
-console.log("Sum = " + friendsData[1].q1 + friendsData[1].q2);
+var newFriend;
+
+var scoresFirst=[];
+var scoresSecond=[];
+var scoresNew=[];
+
+
+
 
 module.exports = function(app) {
- 
 
-app.get("/api/friends", function(req, res) {
+  var newFriend;
+ 
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-// Get all characters
-/*
-app.get("/all", function(req, res) {
-  res.json(friends);
-});
 
-*/
+  app.post("/api/survey", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+    
+      friendsData.push(req.body);
+      res.json(true);
+  });
 
-// Search for Specific Friend - provides JSON
-/*  
+
+  // Search for Specific Friend - provides JSON
   app.get("/api/:friends?", function(req, res) {
     var chosen = req.params.friends;
 
@@ -58,15 +55,6 @@ app.get("/all", function(req, res) {
     }
     return res.json(friends);
   });
-*/
-  app.post("/api/survey", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    
-      friendsData.push(req.body);
-      res.json(true);
-  });
 
 
   // Create New Friend - takes in JSON input
@@ -74,19 +62,111 @@ app.get("/all", function(req, res) {
 
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
-    var newFriend = req.body;
+    newFriend = req.body;
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
 
     console.log(newFriend);
 
-    console.log(newFriend.q1);
-
-
     friendsData.push(newFriend);
 
     res.json(newFriend);
+
+    console.log(friendsData);
+
+    //building scores array for the new friend
+
+    scoresNew.push(parseInt(newFriend.q1));
+    scoresNew.push(parseInt(newFriend.q2));
+    scoresNew.push(parseInt(newFriend.q3));
+    scoresNew.push(parseInt(newFriend.q4));
+    scoresNew.push(parseInt(newFriend.q5));
+    scoresNew.push(parseInt(newFriend.q6));
+    scoresNew.push(parseInt(newFriend.q7));
+    scoresNew.push(parseInt(newFriend.q8));
+    scoresNew.push(parseInt(newFriend.q9));
+    scoresNew.push(parseInt(newFriend.q10));
+
+    console.log("New friend scores: " + scoresNew);
+
+    
+    console.log(friendsData[1].name);
+    numberArray();
+
+    var deltaFirst=0;
+    for (i=0; i<scoresNew.length; i++){
+      deltaFirst+=Math.abs(scoresNew[i]-scoresFirst[i]);
+    }
+    console.log("Roznica pierwsza=" + deltaFirst);
+
+    var deltaSecond=0;
+    for (i=0; i<scoresNew.length; i++){
+      deltaSecond+=Math.abs(scoresNew[i]-scoresSecond[i]);
+    }
+    console.log("Roznica druga=" + deltaSecond);
+
+    if (deltaFirst>=deltaSecond)
+    {
+      console.log("Your best match is " + friendsData[1].name);
+    }
+
+    else {
+      console.log("Your best match is " + friendsData[0].name);
+    }
+
   });
 
 };
+
+
+function numberArray() {
+
+/*
+
+for(var i=0; i<10; i++) {
+    scres[i] = [];
+    for(var j=0; j<9; j++) {
+        scores[i][j] = undefined;
+    }
+}
+
+*/
+
+
+scoresFirst.push(parseInt(friendsData[0].q1));
+scoresFirst.push(parseInt(friendsData[0].q2));
+scoresFirst.push(parseInt(friendsData[0].q3));
+scoresFirst.push(parseInt(friendsData[0].q4));
+scoresFirst.push(parseInt(friendsData[0].q5));
+scoresFirst.push(parseInt(friendsData[0].q6));
+scoresFirst.push(parseInt(friendsData[0].q7));
+scoresFirst.push(parseInt(friendsData[0].q8));
+scoresFirst.push(parseInt(friendsData[0].q9));
+scoresFirst.push(parseInt(friendsData[0].q10));
+
+console.log(scoresFirst);
+
+var sum = scoresFirst[1] + scoresFirst[2];
+
+console.log("Sum = " + sum); 
+
+scoresSecond.push(parseInt(friendsData[1].q1));
+scoresSecond.push(parseInt(friendsData[1].q2));
+scoresSecond.push(parseInt(friendsData[1].q3));
+scoresSecond.push(parseInt(friendsData[1].q4));
+scoresSecond.push(parseInt(friendsData[1].q5));
+scoresSecond.push(parseInt(friendsData[1].q6));
+scoresSecond.push(parseInt(friendsData[1].q7));
+scoresSecond.push(parseInt(friendsData[1].q8));
+scoresSecond.push(parseInt(friendsData[1].q9));
+scoresSecond.push(parseInt(friendsData[1].q10));
+
+console.log(scoresSecond);
+
+var sum = scoresSecond[1] + scoresSecond[2];
+
+console.log("Sum = " + sum); 
+
+}
+
