21921c396c2fee0fa703fc558edccca1c3befacb
Edyta <dudek002@hotmail.com>
2018-03-02 23:37:07 -0600

    working app

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,7 @@ var friendsArray = [
  {
     routeName: "tom",
     name: "Tom",
+    photo: "http://dwgyu36up6iuz.cloudfront.net/heru80fdn/image/upload/c_fill,d_placeholder_thescene.jpg,fl_progressive,g_face,h_450,q_80,w_800/v1489437432/vanityfair_the-cast-of-sesame-street-reads-famous-movie-quotes.jpg",
     q1:1,
     q2:2,
     q3:3,
@@ -16,6 +17,7 @@ var friendsArray = [
   {
     routeName: "mike",
     name: "Mike",
+    photo: "http://news.sagacom.com/wp-content/blogs.dir/3/files/2017/11/cookiemonsterREUTERS.jpg",
     q1:3,
     q2:2,
     q3:3,
@@ -26,6 +28,51 @@ var friendsArray = [
     q8:2,
     q9:4,
     q10:2
+  },
+  {
+    routeName: "laura",
+    name: "Laura",
+    photo: "http://cdn.history.com/sites/2/2017/03/GettyImages-2110712.jpg",
+    q1:5,
+    q2:1,
+    q3:2,
+    q4:2,
+    q5:4,
+    q6:2,
+    q7:3,
+    q8:2,
+    q9:1,
+    q10:1
+  },
+  {
+    routeName: "kate",
+    name: "Kate",
+    photo:"https://www.sesamestreetlive.com/themes/custom/sesame_street_live/assets/images/elmo-footer.png",
+    q1:1,
+    q2:2,
+    q3:3,
+    q4:1,
+    q5:4,
+    q6:1,
+    q7:1,
+    q8:2,
+    q9:4,
+    q10:3
+  },
+  {
+    routeName: "jenny",
+    name: "Jenny",
+    photo:"https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/sesame-SESAMESTREET_36-Full-Image_GalleryBackground-en-US-1483993183108._RI_SX940_.jpg",
+    q1:5,
+    q2:5,
+    q3:3,
+    q4:2,
+    q5:1,
+    q6:5,
+    q7:4,
+    q8:3,
+    q9:1,
+    q10:1
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,54 +1,28 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on friends
-// ===============================================================================
+// =============================================================
 
-// Sets up the Express App
+// Set up the Express App
 // =============================================================
 
 var friendsData = require("../data/friends.js");
-
 var friends = require("../data/friends.js");
-
-var newFriend;
-
-var scoresFirst=[];
-var scoresSecond=[];
-var scoresNew=[];
-
-var lengthFriendsData = 0;
-var columns =[];
-
-
-
 
 module.exports = function(app) {
 
-  var newFriend;
- 
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-
   app.post("/api/survey", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    
-      friendsData.push(req.body);
-      res.json(true);
+    friendsData.push(req.body);
+    res.json(true);
   });
-
 
   // Search for Specific Friend - provides JSON
   app.get("/api/:friends?", function(req, res) {
     var chosen = req.params.friends;
-
     if (chosen) {
       console.log(chosen);
-
       for (var i = 0; i < friends.length; i++) {
         if (chosen === friends[i].routeName) {
           return res.json(friends[i]);
@@ -59,30 +33,19 @@ module.exports = function(app) {
     return res.json(friends);
   });
 
-
   // Create New Friend - takes in JSON input
   app.post("/api/new", function(req, res) {
-
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
-    newFriend = req.body;
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    var newFriend = req.body;
     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(newFriend);
-
+    //console.log(newFriend);
     friendsData.push(newFriend);
-
     res.json(newFriend);
 
+    var scoresNew = [];
+
     console.log(friendsData);
-
-    lengthFriendsData = friendsData.length - 1;
-
-    console.log("Number of friends to compare " + lengthFriendsData);
-
-
 
     //building scores array for the new friend
 
@@ -97,137 +60,45 @@ module.exports = function(app) {
     scoresNew.push(parseInt(newFriend.q9));
     scoresNew.push(parseInt(newFriend.q10));
 
-    console.log("New friend scores: " + scoresNew);
+    console.log("\nNew hiker scores: " + scoresNew);
 
-    
-    console.log(friendsData[1].name);
+    var lengthFriendsData = 0;
+    lengthFriendsData = friendsData.length - 1;
 
+    console.log("\nNumber of hikers to compare: " + lengthFriendsData);
 
-    var numrows = lengthFriendsData;
+    var scores =[,];
+    var winner = 50;
 
-    var numcols = 10;
-
-    
-    var my2Darray = Array.matrix(numrows,numcols,0);
-    
-
-    for (var i = 0; i < numrows; ++i){
-          
-          my2Darray[i][0] = friendsData[i].q1;
-          my2Darray[i][1] = friendsData[i].q2;
-          my2Darray[i][2] = friendsData[i].q3;
-          my2Darray[i][3] = friendsData[i].q4;
-          my2Darray[i][4] = friendsData[i].q5;
-          my2Darray[i][5] = friendsData[i].q6;
-          my2Darray[i][6] = friendsData[i].q7;
-          my2Darray[i][7] = friendsData[i].q8;
-          my2Darray[i][8] = friendsData[i].q9;
-          my2Darray[i][9] = friendsData[i].q10;
-
-          console.log(my2Darray);
-    }
- 
-    console.log(my2Darray); 
-
-
-    numberArray();
-
-    var deltaFirst=0;
-    for (i=0; i<scoresNew.length; i++){
-      deltaFirst+=Math.abs(scoresNew[i]-scoresFirst[i]);
-    }
-    console.log("Roznica pierwsza=" + deltaFirst);
-
-    var deltaSecond=0;
-    for (i=0; i<scoresNew.length; i++){
-      deltaSecond+=Math.abs(scoresNew[i]-scoresSecond[i]);
-    }
-    console.log("Roznica druga=" + deltaSecond);
-
-    if (deltaFirst>=deltaSecond)
+    for (var i=0; i<lengthFriendsData; i++)
     {
-      console.log("The best match for your hiking adventure is " + friendsData[1].name);
-      //alert("The best match for your hiking adventure is " + friendsData[1].name);
-    }
+      scores[i,0] = parseInt(friendsData[i].q1);
+      scores[i,1] = parseInt(friendsData[i].q2);
+      scores[i,2] = parseInt(friendsData[i].q3);
+      scores[i,3] = parseInt(friendsData[i].q4);
+      scores[i,4] = parseInt(friendsData[i].q5);
+      scores[i,5] = parseInt(friendsData[i].q6);
+      scores[i,6] = parseInt(friendsData[i].q7);
+      scores[i,7] = parseInt(friendsData[i].q8);
+      scores[i,8] = parseInt(friendsData[i].q9);
+      scores[i,9] = parseInt(friendsData[i].q10);
 
-    else {
-      console.log("The best match for your hiking adventure is  " + friendsData[0].name);
-      //alert("The best match for your hiking adventure is  " + friendsData[0].name);
+      console.log("\nHiker " + friendsData[i].name + " scores are: " + scores);
+      var delta = 0;
+      for (j=0; j<scoresNew.length; j++){
+      delta+=Math.abs(scoresNew[j]-scores[i,j]);
+      }
+      console.log("Points difference for this hiker is " + delta);
+      if (delta<=winner){
+        winner=delta;
+        winnerName = friendsData[i].name;
+        //console.log("winner =" + winner);
+        //console.log("Companion name " + winnerName);
+        }     
     }
-
+    console.log("\nSmallest points difference for all hikers = " + winner); 
+    console.log("\nThe best match for your hiking adventure is " + winnerName);
   });
-
 };
-
-Array.matrix = function(numrows, numcols, initial){
-  var arr = [];
-  
-  for (var i = 0; i < numrows; ++i){
-    for (var j = 0; j < numcols; ++j){
-      columns[j] = initial;
-    }   
-    arr[i] = columns;
-  }
-  return arr;
-}
-
-
-function numberArray() {
-
-/*
-var scores =[]
-for(var i=0; i<10; i++) {
-    scres[i] = [];
-    for(var j=0; j<9; j++) {
-        scores[i][j] = undefined;
-    }
-}
-*/
-
-
-
-
-
-
-
-
-scoresFirst.push(parseInt(friendsData[0].q1));
-scoresFirst.push(parseInt(friendsData[0].q2));
-scoresFirst.push(parseInt(friendsData[0].q3));
-scoresFirst.push(parseInt(friendsData[0].q4));
-scoresFirst.push(parseInt(friendsData[0].q5));
-scoresFirst.push(parseInt(friendsData[0].q6));
-scoresFirst.push(parseInt(friendsData[0].q7));
-scoresFirst.push(parseInt(friendsData[0].q8));
-scoresFirst.push(parseInt(friendsData[0].q9));
-scoresFirst.push(parseInt(friendsData[0].q10));
-
-console.log(scoresFirst);
-
-//var sum = scoresFirst[1] + scoresFirst[2];
-
-//console.log("Sum = " + sum); 
-
-scoresSecond.push(parseInt(friendsData[1].q1));
-scoresSecond.push(parseInt(friendsData[1].q2));
-scoresSecond.push(parseInt(friendsData[1].q3));
-scoresSecond.push(parseInt(friendsData[1].q4));
-scoresSecond.push(parseInt(friendsData[1].q5));
-scoresSecond.push(parseInt(friendsData[1].q6));
-scoresSecond.push(parseInt(friendsData[1].q7));
-scoresSecond.push(parseInt(friendsData[1].q8));
-scoresSecond.push(parseInt(friendsData[1].q9));
-scoresSecond.push(parseInt(friendsData[1].q10));
-
-console.log(scoresSecond);
-
-//var sum = scoresSecond[1] + scoresSecond[2];
-
-//console.log("Sum = " + sum); 
-
-}
-
-
-
 
 
